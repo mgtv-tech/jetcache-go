f9093e845abbaf54025eb84838d62a4af2e96a19
doashenzzg <daoshenzzg@163.com>
2024-03-05 20:04:32 +0800

    feat: sonic json encoding

---
+++ encoding/sonic/sonic.go
@@ -0,0 +1,28 @@
+package sonic
+
+import (
+	"github.com/bytedance/sonic"
+	"github.com/daoshenzzg/jetcache-go/encoding"
+)
+
+// Name is the name registered for the json codec.
+const Name = "sonic"
+
+func init() {
+	encoding.RegisterCodec(codec{})
+}
+
+// codec is a Codec implementation with json.
+type codec struct{}
+
+func (codec) Marshal(v interface{}) ([]byte, error) {
+	return sonic.Marshal(v)
+}
+
+func (codec) Unmarshal(data []byte, v interface{}) error {
+	return sonic.Unmarshal(data, v)
+}
+
+func (codec) Name() string {
+	return Name
+}
